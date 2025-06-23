30f433356fa7afbb27bcf821a9115e1638d2e5f8
peter <peterpravin2506@gmail.com>
2025-06-23 07:19:55 +0530

    Fetching Data from the DAO and Forwarding to JSP

---
+++ src/main/java/servlet/MovieServlet.java
@@ -0,0 +1,20 @@
+package servlet;
+
+import dao.MovieDAO;
+import model.Movie;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+
+public class MovieServlet extends HttpServlet{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        MovieDAO movieDAO=new MovieDAO();
+        List<Movie> movies = movieDAO.getAllMovies();
+
+        request.setAttribute("movieList",movies);
+        RequestDispatcher dispatcher=request.getRequestDispatcher("pages/movies.jsp");
+        dispatcher.forward(request,response);
+    }
+}
